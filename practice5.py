2a164c7917e59ea9c818ed56b260a1fc18cbc5c1
ManishKumarGupta <168831259+ManishKumarGupta88@users.noreply.github.com>
2025-02-03 22:26:42 +0530

    Create practice5.py

---
+++ practice5.py
@@ -0,0 +1,28 @@
+#OPERATORS IN PYTHON
+#Arithmetic operators: +, -, *, / etc.
+a=2; b=6
+a=a+b
+c=a-b
+d=a*b
+e=b/a
+print(c,a,b,type(c),d,e)
+#Assignment operators: =, +=, -= etc.
+a=a+1
+a+=b
+b-=c
+print(a,b)
+#Comparison operators: ==, >, >=, <, != etc
+if(a==b):
+    print("a and b are equal.")
+else:
+    print("a and b are not equal")
+if(a>=b):
+    print("a grater than b.")
+else:
+    print("a smaller then b")
+#same as lower then
+#. Logical operators: and, or, not.
+if(a & b !=  b|c):
+    print("a and b are not equal to b or c")
+
+
