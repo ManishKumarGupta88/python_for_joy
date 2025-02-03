5f1666cd4c2dd637c4a4c9164d473dc0f5a3f2ef
ManishKumarGupta <168831259+ManishKumarGupta88@users.noreply.github.com>
2025-02-03 22:25:57 +0530

    Create practice4.py

---
+++ practice4.py
@@ -0,0 +1,7 @@
+#variables types
+a=1 # a is integer
+b=5.22 #b is floating type
+c="manish" #c is string
+d=False #d id bool type
+e=None #non type define variable
+print(a,type(a),b,type(b),c,type(c),d,type(d),e,type(e))
